904d2771b6d518def789018db3f68574b5150a26
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-11-01 20:56:10 +0200

    Add UserRepo with basic user CRUD methods
    Introduces the UserRepo struct with methods to create a user and retrieve users by ID, email, or name. This provides a repository abstraction for user-related database operations using the application's DB configuration.

--- internal/repositories/user_repo.go
+++ internal/repositories/user_repo.go
@@ -1,1 +1,46 @@
 package repositories
+
+import (
+	"FitClassMaster/internal/config"
+	"FitClassMaster/internal/models"
+)
+
+type UserRepo struct{}
+
+func NewUserRepo() *UserRepo {
+	return &UserRepo{}
+}
+
+func (r *UserRepo) Create(u *models.User) error {
+	return config.DB.Create(u).Error
+}
+
+func (r *UserRepo) GetById(id int) (user *models.User, err error) {
+	var u models.User
+
+	if err := config.DB.First(&u, id).Error; err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+func (r *UserRepo) GetByEmail(email string) (user *models.User, err error) {
+	var u models.User
+
+	if err := config.DB.Where("name=?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+func (r *UserRepo) GetByName(name string) (user *models.User, err error) {
+	var u models.User
+
+	if err := config.DB.Where("name = ?", name).First(&u).Error; err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
