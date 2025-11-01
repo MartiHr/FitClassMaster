c83f870653e58dff480c0796a79cd97d05e115dd
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-11-01 23:43:04 +0200

    Add AuthHandler and register page handler
    Introduces the AuthHandler struct with dependencies on AuthService and UserRepo. Adds initialization logic and a RegisterPage handler to render the registration template.

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,1 +1,37 @@
 package handlers
+
+import (
+	"FitClassMaster/internal/repositories"
+	"FitClassMaster/internal/services"
+	"FitClassMaster/internal/templates"
+	"encoding/gob"
+	"html/template"
+	"net/http"
+)
+
+var authTemplates *template.Template
+
+func init() {
+	gob.Register(uint(0)) // so session can store user ID
+}
+
+type AuthHandler struct {
+	AuthService *services.AuthService
+	Repo        *repositories.UserRepo
+}
+
+func NewAuthHandler() *AuthHandler {
+	repo := repositories.NewUserRepo()
+	authService := services.NewAuthService(repo)
+
+	return &AuthHandler{AuthService: authService, Repo: repo}
+}
+
+// RegisterPage (GET)
+func (h *AuthHandler) RegisterPage(w http.ResponseWriter, r *http.Request) {
+	templates.Render(w, "register.gohtml", map[string]any{
+		"Title": "Register | FitClassMaster",
+	})
+}
+
+//
