320a3649e304b3fe0ccdabc8d65501338527637b
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-11-01 21:28:58 +0200

    Add AuthService with register and login methods
    Introduces AuthService to handle user registration and login. Registration checks for existing email, hashes passwords, and stores users. Login verifies credentials using bcrypt. Depends on UserRepo for data access.

--- internal/services/auth_service.go
+++ internal/services/auth_service.go
@@ -1,1 +1,49 @@
 package services
+
+import (
+	"FitClassMaster/internal/models"
+	"FitClassMaster/internal/repositories"
+
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService struct {
+	repo *repositories.UserRepo
+}
+
+func NewAuthService(r *repositories.UserRepo) *AuthService {
+	return &AuthService{repo: r}
+}
+
+func (as *AuthService) Register(u *models.User, plainPassword string) error {
+	// check whether it exists
+	if _, err := as.repo.GetByEmail(u.Email); err == nil {
+		return errors.New("email already used")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(plainPassword), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hash)
+
+	return as.repo.Create(u)
+}
+
+func (as *AuthService) Login(email, password string) (*models.User, error) {
+	user, err := as.repo.GetByEmail(email)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
